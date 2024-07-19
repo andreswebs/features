f46af05e685a3ae6a3c8e60e92edacd564a5aa16
Andre Silva <andreswebs@pm.me>
2024-07-19 16:23:40 -0300

    fix install

--- src/nix/install.sh
+++ src/nix/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix > nix-installer.sh
 chmod +x nix-installer.sh
-./nix-installer.sh install
+./nix-installer.sh install -—no-confirm --init none
 rm nix-installer.sh
