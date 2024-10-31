f6868cfb2caeb791ad45d9dd2b7ebe79ee12d38e
Andre Silva <andreswebs@pm.me>
2024-10-31 01:04:06 -0300

    fix starship; make zsh-starship idempotent

--- src/zsh-starship/install.sh
+++ src/zsh-starship/install.sh
@@ -1,12 +1,40 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-curl -sS https://starship.rs/install.sh | sh
-echo "eval \"\$(starship init zsh)\"" >> "${HOME}/.zshrc"
+if ! command -v starship &> /dev/null; then
+  curl -fsSL https://starship.rs/install.sh | sh -s -- --yes
+fi
 
-git clone https://github.com/zsh-users/zsh-autosuggestions "${HOME}/.zsh/zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${HOME}/.zsh/zsh-syntax-highlighting"
+if ! grep -q 'eval "$(starship init zsh)"' "${HOME}/.zshrc"; then
+  echo >> "${HOME}/.zshrc"
+  cat << 'EOT' >> "${HOME}/.zshrc"
+if command -v starship &> /dev/null; then
+  eval "$(starship init zsh)"
+fi
+EOT
+fi
 
-echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> "${HOME}/.zshrc"
-echo "source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "${HOME}/.zshrc"
+if [ ! -d "${HOME}/.zsh/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions "${HOME}/.zsh/zsh-autosuggestions"
+fi
 
-# mkdir -p ~/.config && printf "[container]\ndisabled = true" >> ~/.config/starship.toml
+if ! grep -q 'source "${HOME}/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"' "${HOME}/.zshrc"; then
+  echo >> "${HOME}/.zshrc"
+  cat << 'EOT' >> "${HOME}/.zshrc"
+if [ -d "${HOME}/.zsh/zsh-autosuggestions" ]; then
+  source "${HOME}/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"
+fi
+EOT
+fi
+
+if [ ! -d "${HOME}/.zsh/zsh-syntax-highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${HOME}/.zsh/zsh-syntax-highlighting"
+fi
+
+if ! grep -q 'source "${HOME}/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"' "${HOME}/.zshrc"; then
+  echo >> "${HOME}/.zshrc"
+  cat << 'EOT' >> "${HOME}/.zshrc"
+if [ -d "${HOME}/.zsh/zsh-syntax-highlighting" ]; then
+  source "${HOME}/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+fi
+EOT
+fi
