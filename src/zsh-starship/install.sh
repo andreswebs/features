4e81dee2f6bdaa600788f5ddac8021b35cb051b2
Andre Silva <andreswebs@pm.me>
2024-10-31 15:46:08 -0300

    fix

--- src/zsh-starship/install.sh
+++ src/zsh-starship/install.sh
@@ -1,45 +1,56 @@
 #!/usr/bin/env sh
 
-mkdir --parents "${HOME}/.zsh"
-touch "${HOME}/.zshrc"
+TARGET_HOME="${HOME}"
 
-STARSHIP_BIN_DIR="${STARSHIP_BIN_DIR:-/usr/local/bin}"
+if [ -n "${_REMOTE_USER_HOME}" ]; then
+  TARGET_HOME="${_REMOTE_USER_HOME}"
+fi
+
+mkdir --parents "${TARGET_HOME}/.zsh"
+touch "${TARGET_HOME}/.zshrc"
+
+STARSHIP_BIN_DIR="${STARSHIP_BIN_DIR:-'/usr/local/bin'}"
 
 if ! command -v starship &> /dev/null; then
   curl -fsSL https://starship.rs/install.sh | sh -s -- --yes --bin-dir "${STARSHIP_BIN_DIR}"
 fi
 
-if ! grep -q 'eval "$(starship init zsh)"' "${HOME}/.zshrc"; then
-  echo >> "${HOME}/.zshrc"
-  cat << 'EOT' >> "${HOME}/.zshrc"
+if ! grep -q 'eval "$(starship init zsh)"' "${TARGET_HOME}/.zshrc"; then
+  echo >> "${TARGET_HOME}/.zshrc"
+  cat << 'EOT' >> "${TARGET_HOME}/.zshrc"
 if command -v starship &> /dev/null; then
   eval "$(starship init zsh)"
 fi
 EOT
 fi
 
-if [ ! -d "${HOME}/.zsh/zsh-autosuggestions" ]; then
-  git clone https://github.com/zsh-users/zsh-autosuggestions "${HOME}/.zsh/zsh-autosuggestions"
+if [ ! -d "${TARGET_HOME}/.zsh/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions "${TARGET_HOME}/.zsh/zsh-autosuggestions"
 fi
 
-if ! grep -q 'source "${HOME}/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"' "${HOME}/.zshrc"; then
-  echo >> "${HOME}/.zshrc"
-  cat << 'EOT' >> "${HOME}/.zshrc"
+if ! grep -q 'source "${HOME}/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"' "${TARGET_HOME}/.zshrc"; then
+  echo >> "${TARGET_HOME}/.zshrc"
+  cat << 'EOT' >> "${TARGET_HOME}/.zshrc"
 if [ -d "${HOME}/.zsh/zsh-autosuggestions" ]; then
   source "${HOME}/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"
 fi
 EOT
 fi
 
-if [ ! -d "${HOME}/.zsh/zsh-syntax-highlighting" ]; then
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${HOME}/.zsh/zsh-syntax-highlighting"
+if [ ! -d "${TARGET_HOME}/.zsh/zsh-syntax-highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${TARGET_HOME}/.zsh/zsh-syntax-highlighting"
 fi
 
-if ! grep -q 'source "${HOME}/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"' "${HOME}/.zshrc"; then
-  echo >> "${HOME}/.zshrc"
-  cat << 'EOT' >> "${HOME}/.zshrc"
+if ! grep -q 'source "${HOME}/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"' "${TARGET_HOME}/.zshrc"; then
+  echo >> "${TARGET_HOME}/.zshrc"
+  cat << 'EOT' >> "${TARGET_HOME}/.zshrc"
 if [ -d "${HOME}/.zsh/zsh-syntax-highlighting" ]; then
   source "${HOME}/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 fi
 EOT
+fi
+
+if [ -n "${_REMOTE_USER}" ]; then
+  chown -R "${_REMOTE_USER}:${_REMOTE_USER}" "${TARGET_HOME}/.zsh"
+  chown "${_REMOTE_USER}:${_REMOTE_USER}" "${TARGET_HOME}/.zshrc"
 fi
